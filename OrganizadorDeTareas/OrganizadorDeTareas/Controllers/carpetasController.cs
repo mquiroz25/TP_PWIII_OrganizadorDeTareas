73b8065aee2587e77c62ae3a71910364767e70fa
mquiroz25 <mquiroz2535@gmail.com>
2018-05-14 11:11:01 -0300

    creacion de  carpetas ,falta mostrarlas en una vista

---
+++ OrganizadorDeTareas/OrganizadorDeTareas/Controllers/carpetasController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using OrganizadorDeTareas.Models;
+
+namespace OrganizadorDeTareas.Controllers
+{
+    public class CarpetasController : Controller
+    {
+        // GET: carpetas
+        public ActionResult Crear()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Carpeta carpeta)
+        {
+         
+               var path= Server.MapPath("~/MisCarpetas/"+carpeta.Nombre);
+
+            Directory.CreateDirectory(path);
+
+            return RedirectToAction("MisCarpetasCreadas", "carpetas");//accion,controller
+        }
+
+           public ActionResult MisCarpetasCreadas()
+        {
+        
+            return View();
+
+        }
+    }
+}
---
+++ OrganizadorDeTareas/OrganizadorDeTareas/Models/Carpeta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrganizadorDeTareas.Models
+{
+    public class Carpeta
+    {
+        public string Nombre { get; set; }
+
+    }
+}
