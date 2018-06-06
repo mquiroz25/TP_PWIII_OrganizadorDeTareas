c943ab6cac99824ce6012f6efee1626d395e0279
mquiroz25 <mquiroz2535@gmail.com>
2018-06-06 15:37:11 -0300

    abm carpetas funcionando

--- OrganizadorDeTareas/OrganizadorDeTareas/Controllers/carpetasController.cs
+++ OrganizadorDeTareas/OrganizadorDeTareas/Controllers/carpetasController.cs
@@ -10,6 +10,7 @@ namespace OrganizadorDeTareas.Controllers
 {
     public class CarpetasController : Controller
     {
+        PW3TP_20181C_TareasEntities CTX = new PW3TP_20181C_TareasEntities();
         // GET: carpetas
         public ActionResult index()
         {
@@ -17,7 +18,75 @@ namespace OrganizadorDeTareas.Controllers
 
         }
 
+        public ActionResult crear()
+        {
+            return View();
 
-  
+        }
+
+        [HttpPost]
+        public ActionResult crear(Carpeta carpeta)
+
+        {
+            carpeta.FechaCreacion = DateTime.Now;
+            CTX.Carpeta.Add(carpeta);
+            CTX.SaveChanges();
+
+            return RedirectToAction("Listar");
+
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Carpeta carpeta = CTX.Carpeta.FirstOrDefault(o => o.IdCarpeta == id);
+
+            if (carpeta != null)
+            {
+                return View(carpeta);
+
+            }
+            return RedirectToAction("Listar");
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Carpeta carpeta)
+
+        {
+            Carpeta carpActual = CTX.Carpeta.FirstOrDefault(o => o.IdCarpeta == carpeta.IdCarpeta);
+
+            if (carpActual != null)
+            {
+                carpActual.Nombre = carpeta.Nombre;
+                carpActual.Descripcion = carpeta.Descripcion;
+
+                CTX.SaveChanges();
+                return RedirectToAction("Listar");
+            }
+
+            return View(carpeta);
+        }
+
+
+        public ActionResult Borrar(int id)
+        {
+            Carpeta carpeta = CTX.Carpeta.FirstOrDefault(o => o.IdCarpeta == id);
+
+            if (carpeta != null)
+            {
+                CTX.Carpeta.Remove(carpeta);
+                CTX.SaveChanges();
+            }
+
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Listar()
+        {
+
+            List<Carpeta> carpetas = CTX.Carpeta.ToList();
+
+            return View(carpetas);
+        }
+
     }
 }
