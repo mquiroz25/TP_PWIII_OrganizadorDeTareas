5a58f342e157a3c2de23a2ee6591baf61187ceef
pabesuarez <pabesuarez@gmail.com>
2018-06-09 02:17:45 -0300

    Revert "avance"
    This reverts commit 4fac59f7182fed0046c925ab474d132853abb960.

--- OrganizadorDeTareas/OrganizadorDeTareas/Controllers/HomeController.cs
+++ OrganizadorDeTareas/OrganizadorDeTareas/Controllers/HomeController.cs
@@ -3,74 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using OrganizadorDeTareas.Models;
 
 namespace OrganizadorDeTareas.Controllers
 {
     public class HomeController : Controller
     {
-
-        PW3TP_20181C_TareasEntities ctx = new PW3TP_20181C_TareasEntities();
         // GET: Home
         public ActionResult index()
         {
             return View();
         }
 
-
         public ActionResult logueado(Usuario u)
         {
-            Session["mail"] = u.Email;
+            Session["mail"] = u.email;
             return RedirectToAction("index", "home"); 
+
+        
         }
 
         public ActionResult registracion()
         {
             return View();
         }
-
-        [HttpPost]
-        public ActionResult Registro(Usuario u)
-        {
-            Usuario nuevo = new Usuario();
-            nuevo.Nombre = u.Nombre;
-            nuevo.Apellido = u.Apellido;
-            nuevo.Contrasenia = u.Contrasenia;
-            nuevo.Email = u.Email;
-            nuevo.FechaRegistracion = DateTime.Now;
-            nuevo.FechaActivacion = DateTime.Now;
-            nuevo.CodigoActivacion = "aaaaa";
-            nuevo.Activo = 1;
-            ctx.Usuario.Add(nuevo);
-            ctx.SaveChanges();
-
-            return RedirectToAction("index", "home");
-        }
-
         public ActionResult login()
         {
             return View();
-        }
-
-        [ActionName("login")]
-        [HttpPost]
-        public ActionResult validarLogin()
-        {
-            String loginMail = Request["Email"];
-            String loginPass = Request["Contrasenia"];
-            Usuario u = ctx.Usuario.SingleOrDefault(o => o.Email == loginMail && o.Contrasenia == loginPass);
-            if (!Object.ReferenceEquals(null, u))
-            {
-                Session["mail"] = u.Email;
-                Session["usuarioid"] = u.IdUsuario;
-                return RedirectToAction("index", "home");
-            }
-            else
-            {
-                TempData["mensaje"] = "usuario o contraseña incorrecto";
-                return View();
-            }
-            
         }
 
         public ActionResult logout()
--- OrganizadorDeTareas/OrganizadorDeTareas/Controllers/carpetasController.cs
+++ OrganizadorDeTareas/OrganizadorDeTareas/Controllers/carpetasController.cs
@@ -4,142 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
-
+using OrganizadorDeTareas.Models;
 
 namespace OrganizadorDeTareas.Controllers
 {
     public class CarpetasController : Controller
     {
-        PW3TP_20181C_TareasEntities CTX = new PW3TP_20181C_TareasEntities();
         // GET: carpetas
         public ActionResult index()
         {
-            if (Session["usuarioid"] != null)
-            {
-                return View();
-            }
-            else
-            {
-                TempData["mensaje"] = "login requerido";
-                return RedirectToAction("login", "home");
-            }
+            return View();
 
         }
 
-        public ActionResult crear()
-        {
-            if (Session["usuarioid"] != null)
-            {
-                return View();
-            }
-            else
-            {
-                TempData["mensaje"] = "login requerido";
-                return RedirectToAction("login", "home");
-            }
 
-        }
-
-        [HttpPost]
-        public ActionResult crear(Carpeta carpeta)
-
-        {
-            if (Session["usuarioid"] != null)
-            {
-                carpeta.FechaCreacion = DateTime.Now;
-                carpeta.IdUsuario = (int)Session["usuarioid"];
-                CTX.Carpeta.Add(carpeta);
-                CTX.SaveChanges();
-                return RedirectToAction("Listar");
-            }
-            else
-            {
-                TempData["mensaje"] = "login requerido";
-                return RedirectToAction("login", "home");
-            }
-        }
-
-        public ActionResult Editar(int id)
-        {
-            if (Session["usuarioid"] != null)
-            {
-                Carpeta carpeta = CTX.Carpeta.FirstOrDefault(o => o.IdCarpeta == id);
-
-                if (carpeta != null)
-                {
-                    return View(carpeta);
-
-                }
-                return RedirectToAction("Listar");
-            }
-            else
-            {
-                TempData["mensaje"] = "login requerido";
-                return RedirectToAction("login", "home");
-            }
-        }
-
-        [HttpPost]
-        public ActionResult Editar(Carpeta carpeta)
-
-        {
-            Carpeta carpActual = CTX.Carpeta.FirstOrDefault(o => o.IdCarpeta == carpeta.IdCarpeta);
-
-            if (carpActual != null)
-            {
-                carpActual.Nombre = carpeta.Nombre;
-                carpActual.Descripcion = carpeta.Descripcion;
-                CTX.SaveChanges();
-                return RedirectToAction("Listar");
-            }
-
-            return View(carpeta);
-        }
-
-
-        public ActionResult Borrar(int id)
-        {
-            if (Session["usuarioid"] != null)
-            {
-                Carpeta carpeta = CTX.Carpeta.FirstOrDefault(o => o.IdCarpeta == id);
-
-                if (carpeta != null)
-                {
-                    if (carpeta.IdUsuario == (int)Session["usuarioid"])
-                    {
-                        CTX.Carpeta.Remove(carpeta);
-                        CTX.SaveChanges();
-                    }
-                }
-
-                return RedirectToAction("Listar");
-            }
-            else
-            {
-                TempData["mensaje"] = "login requerido";
-                return RedirectToAction("login", "home");
-            }
-
-
-
-        }
-
-        public ActionResult Listar()
-        {
-            if (Session["usuarioid"] != null)
-            {
-                List<Carpeta> carpetas = CTX.Carpeta.ToList();
-                carpetas = carpetas.Where(o => o.IdUsuario == (int)Session["usuarioid"]).ToList<Carpeta>();
-
-                return View(carpetas);
-            }
-            else
-            {
-                TempData["mensaje"] = "login requerido";
-                return RedirectToAction("login", "home");
-            }
-
-        }
-
+  
     }
 }
